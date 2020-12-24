44db57aa12b74f0ebb7e695c9ddf7dbbd2f795d2
Nikita-xz <72936543+Nikita-xz@users.noreply.github.com>
2020-12-25 00:53:57 +0530

    my second commit
    my second commit

---
+++ src/com/xworkz/alphabets/L.java
@@ -0,0 +1,20 @@
+package com.xworkz.alphabets;
+
+public class L {
+
+	public static void main(String[] args) {
+		int num=4;
+		for(int i=0;i<=num;i++) {
+			for(int j=0;j<=num;j++) {
+				if(j==0 || i==4)
+					System.out.print("* ");
+				else
+					System.out.print(" ");
+		}
+			System.out.println();
+	}
+
+
+	}
+
+}
---
+++ src/com/xworkz/dimentationalArray/SimpleTwoDArray.java
@@ -0,0 +1,13 @@
+package com.xworkz.dimentationalArray;
+
+import java.util.Arrays;
+
+public class SimpleTwoDArray {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Arrays[][] arr=new Arrays[2][2];
+		arr[0][0]=1;
+		
+
+	}
+}
